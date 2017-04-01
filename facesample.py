0f87872f247a22e4d6a1d2a2affdc780f6be4401
Matt Zamora <matt@mocomakers.com>
2017-04-01 00:51:32 -0400

    Commit of Facial tracking sample

---
+++ facesample.py
@@ -0,0 +1,65 @@
+import cv2
+import sys
+
+import socket
+
+import urllib2
+
+from PIL import Image
+import io
+import httplib
+
+import numpy as np
+import urllib
+
+cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_URL="http://10.0.0.43:8080/video"
+
+#video_capture = cv2.VideoCapture('0')
+
+def grabImage4(video_URL):
+    stream=urllib.urlopen(video_URL)
+    bytes=''
+    while True:
+        bytes+=stream.read(1024)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if a!=-1 and b!=-1:
+            jpg = bytes[a:b+2]
+            bytes= bytes[b+2:]
+            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
+            #print "Found Image"
+	    return i
+
+while True:
+    # Capture frame-by-frame
+    #ret, frame = video_capture.read()
+    image = grabImage4(video_URL)
+    frame = image
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+#video_capture.release()
+cv2.destroyAllWindows()
+
